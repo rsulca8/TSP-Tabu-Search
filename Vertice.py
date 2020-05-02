5ceba32266e94dc0611f4ac687783d2251d5ad9d
Rodrigo Sulca <rsulca8@gmail.com>
2020-05-02 13:29:35 -0300

    Creacion de la clase Vertice con Getter y Setter

---
+++ Vertice.py
@@ -0,0 +1,21 @@
+class Vertice():
+
+    def __init__(self,V, N):
+        self._value = V
+        self._next = N
+
+    def getValue(self):
+        return self._value
+
+    def getNext(self):
+        return self._next
+    
+    def setValue(self, V):
+        self._value = V
+    
+    def setNext(self, N):
+        self._next = N
+
+    
+        
+
