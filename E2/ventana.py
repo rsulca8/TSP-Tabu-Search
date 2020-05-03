2e92ef409230081d6da374be38a298d820eeddde
Rodrigo Sulca <rsulca8@gmail.com>
2020-05-03 19:38:47 -0300

    permitir cargar un grafo a partir de un EUC_2D y poder mostrar grafo

--- E2/ventana.py
+++ E2/ventana.py
@@ -1,14 +1,16 @@
 import tkinter as tk
 from Grafo import Grafo 
-
+from Table import Table
+from Vertice import Vertice
 import tkinter.filedialog
 
 class Ventana(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
-        self.geometry("600x800")
+        self.geometry("300x300")
         self.titulo()
         self.barraMenus()
+        self.menuGrafo()
     
     def titulo(self):
         self.__labelTitulo = tk.Label(self,text = "TSP Solver con Tabu Search")
@@ -20,12 +22,44 @@ class Ventana(tk.Tk):
         self.__menuArchivo.add_command(label="Abrir", command=self.openFile)
         self.__menu.add_cascade(label="Archivo", menu=self.__menuArchivo)
         self.config(menu = self.__menu)
+    
+    def menuGrafo(self):
+        self.__labelEstadoGrafo = tk.Label(self, text = "No se ha cargado Grafo")
+        self.__botonMostrarGrafo = tk.Button(self, text = "Mostrar Grafo", command=self.mostrarGrafo,state="disabled")
+        self.__labelEstadoGrafo.pack(fill=tk.X, padx=40)
+        self.__botonMostrarGrafo.pack(fill=tk.X, padx=40)
 
     def openFile(self):
         self.__nombreArchivo  = tk.filedialog.askopenfilename(initialdir = ".",title = "Select file",filetypes = (("all files","*.*"),("jpeg files","*.jpg")))
         print(self.__nombreArchivo)
         self.__g = Grafo(self.__nombreArchivo)
+        self.__labelEstadoGrafo.configure(text = "Grafo Cargado")
+        self.__botonMostrarGrafo.configure(state="normal")
         print(self.__g)
+
+    def mostrarGrafo(self):
+        self.__ventanaTabla = tk.Toplevel(self)
+        self.__ventanaTabla.title("Grafo Cargado")
+        self.__ventanaTabla.geometry('500x500')
+        
+        #vertices_header = tuple(i for i in str(self.__g.getV()[i].getValue()))
+        #print(vertices_header)
+        vertices_header=verticesATupla([Vertice(" ")]+self.__g.getV())
+        tabla = Table(self.__ventanaTabla, title="Vertices", headers=vertices_header)
+        M = self.__g.getMatriz()
+        for i in range(0,len(M)):
+            fila = []
+            for j in range(0,len(M)):
+                fila.append(M[i][j])
+            tabla.add_row(fila)
+        tabla.pack()
+
+
+def verticesATupla(V):
+    v = []
+    for i in V:
+        v.append(str(i.getValue()))
+    return v
 
 ventana = Ventana()
 
