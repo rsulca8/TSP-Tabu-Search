5f3b9e1192425fa165f676b6c38c2a2e68d21149
Rodrigo Sulca <rsulca8@gmail.com>
2020-05-04 12:11:48 -0300

    nada nuevo

--- E2/main.py
+++ E2/main.py
@@ -8,13 +8,22 @@ if __name__ == "__main__":
     #Creación del Grafo
     g = G("eil101.tsp")
 
+    v1 = V(1)
+    v2 = V(2)
+    v3 = V(3)
+    v4 = V(4)
+
+    a1 = Arista(v1,v2,0)
+    a2 = Arista(v2,v1,0)
+    a3 = Arista(v2,v3,0)
+    a4 = Arista(v1,v3,0)
 
 
-    
     print("------------------ CARGAR DESDE MATRIZ --------------------------")
     #g.obtenerSolucionVecinoCercano(V(1))
     print("NODOS CON ORIGEN")
-    print("A: " + str(g.getA()) + "\n" +"\n")
+
+    
     print(g.obtenerSolucionVecinoCercano(V(1)))
 
     #print("grafo g: " + str(g))
